abffc81af35661cd69f3be93f0e3adc75fa0e07f
Samuel Farrell <samuelfarrell@me.com>
2020-04-22 13:38:57 -0500

    add assets

--- dirigible-seo.php
+++ dirigible-seo.php
@@ -1,7 +1,7 @@
 <?php
 /*
  Plugin Name: Dirigible SEO
- Plugin URI: https://dirigiblestudio.com
+ Plugin URI: https://dirigiblestudio.com/wordpress/plugins/
  description: Dead simple SEO Control for Wordpress. Requires ACF.
  Version: 1.0.0
  Author: Dirigible Studio
