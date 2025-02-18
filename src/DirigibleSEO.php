838869fdc9a74983efbacfc97f3ef767eeea431c
Jake Casey <acasestudy0@gmail.com>
2025-02-18 11:57:31 -0600

    remove second name property on og:description tag

--- src/DirigibleSEO.php
+++ src/DirigibleSEO.php
@@ -151,7 +151,7 @@ class DirigibleSEO
   {
     $description = $this->stringFilters($this->metaDescription());
     echo '<meta name="description" content="' . $description . '">';
-    echo '<meta property="og:description" name="description" content="' . $description . '">';
+    echo '<meta property="og:description" content="' . $description . '">';
   }
 
   public function printMetaImageTag()
